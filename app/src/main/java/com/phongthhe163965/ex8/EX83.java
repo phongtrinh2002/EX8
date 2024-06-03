68d1c68d83c5a91ae8e039bea789763aabd7aaed
phongtrinh2002 <122010784+phongtrinh2002@users.noreply.github.com>
2024-06-04 00:46:21 +0700

    Initial commit

---
+++ app/src/main/java/com/phongthhe163965/ex8/EX83.java
@@ -0,0 +1,18 @@
+package com.phongthhe163965.ex8;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EX83 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex83);
+    }
+}
